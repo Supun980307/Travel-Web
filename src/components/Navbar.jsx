43b009f256757843e1e2d4a75f3d2767ce144037
Supun Madusankha <76279098+Supun980307@users.noreply.github.com>
2023-10-23 01:22:49 +0530

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { BsPerson } from 'react-icons/bs';
 import { BiSearch } from 'react-icons/bi';
+import { BsPerson } from 'react-icons/bs';
 import { AiOutlineClose } from 'react-icons/ai';
 import { HiOutlineMenuAlt4 } from 'react-icons/hi';
 import {
@@ -12,59 +12,84 @@ import {
 } from 'react-icons/fa';
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false);
-  const [logo, setLogo] = useState(false)
-  const handleNav = () => {
-    setNav(!nav);
-    setLogo(!logo)
+  const [navOpen, setNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setNavOpen(!navOpen);
+  };
+
+  const beachRiderStyle = {
+    fontSize: '2rem', 
+    fontFamily: 'cursive', 
+    color: 'black',
+    
   };
 
   return (
-    <div className='flex w-full justify-between items-center h-20 px-4 absolute z-10 text-white'>
-      <div>
-        <h1 onClick={handleNav} className={logo ? 'hidden' : 'block'}>BEACHES.</h1>
+    <nav className='bg-gradient-to-r from-blue-500 to-purple-500 text-white py-5 px-4 md:px-10 flex justify-between items-center z-10'>
+      <div className='text-2xl font-bold cursor-pointer'>
+        <h1 onClick={toggleNav} className={navOpen ? 'hidden' : 'block'} style={beachRiderStyle}>
+          BEACH.Rider
+        </h1>
       </div>
-      <ul className='hidden md:flex'>
-        <li>Home</li>
-        <li>Destinations</li>
-        <li>Travel</li>
-        <li>View</li>
-        <li>Book</li>
+      <ul className='hidden md:flex space-x-6 text-lg'>
+        <li className='hover:text-blue-800 transition duration-300'>Home</li>
+        <li className='hover:text-blue-800 transition duration-300'>Destinations</li>
+        <li className='hover:text-blue-800 transition duration-300'>Travel</li>
+        <li className='hover:text-blue-800 transition duration-300'>View</li>
+        <li className='hover:text-blue-800 transition duration-300'>Book</li>
       </ul>
-      <div className='hidden md:flex'>
-        <BiSearch className='' size={20} />
-        <BsPerson size={20} />
+      <div className='hidden md:flex space-x-6'>
+        <BiSearch size={24} />
+        <BsPerson size={24} />
       </div>
 
       {/* Hamburger */}
-      <div onClick={handleNav} className='md:hidden z-10'>
-        {nav ? <AiOutlineClose className='text-black' size={20} /> : <HiOutlineMenuAlt4 size={20} />}
+      <div onClick={toggleNav} className='md:hidden z-10'>
+        {navOpen ? (
+          <AiOutlineClose className='text-2xl' />
+        ) : (
+          <HiOutlineMenuAlt4 size={24} />
+        )}
       </div>
 
       {/* Mobile menu dropdown */}
-      <div onClick={handleNav} className={nav ? 'absolute text-black left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col' : 'absolute left-[-100%]'}>
-        <ul>
-          <h1>BEACHES.</h1>
-          <li className='border-b'>Home</li>
-          <li className='border-b'>Destinations</li>
-          <li className='border-b'>Travel</li>
-          <li className='border-b'>View</li>
-          <li className='border-b'>Book</li>
-          <div className='flex flex-col'>
-            <button className='my-6'>Search</button>
-            <button>Account</button>
-          </div>
-          <div className='flex justify-between my-6'>
-            <FaFacebook className='icon' />
-            <FaTwitter className='icon' />
-            <FaYoutube className='icon' />
-            <FaPinterest className='icon' />
-            <FaInstagram className='icon' />
-          </div>
+      <div
+        onClick={toggleNav}
+        className={`${
+          navOpen
+            ? 'absolute text-black left-0 top-0 w-full bg-white/90 px-4 py-7 flex flex-col space-y-4'
+            : 'absolute left-[-100%]'
+        } md:hidden`}
+      >
+        <h1 className='text-2xl font-bold' style={beachRiderStyle}>BEACH.Rider</h1>
+        <ul className='space-y-2'>
+          <li className='hover:text-blue-800 transition duration-300'>Home</li>
+          <li className='hover:text-blue-800 transition duration-300'>Destinations</li>
+          <li className='hover:text-blue-800 transition duration-300'>Travel</li>
+          <li className='hover:text-blue-800 transition duration-300'>View</li>
+          <li className='hover:text-blue-800 transition duration-300'>Book</li>
         </ul>
+        <div className='flex flex-col space-y-4'>
+          <button className='py-2 px-6 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition duration-300'>
+            Search
+          </button>
+          <button className='py-2 px-6 bg-purple-500 text-white rounded-full hover:bg-purple-600 transition duration-300'>
+            Account
+          </button>
+        </div>
+        <div className='flex justify-center space-x-6 my-4'>
+          <FaFacebook className='icon text-blue-600 hover:text-blue-800' />
+          <FaTwitter className='icon text-blue-400 hover:text-blue-600' />
+          <FaYoutube className='icon text-red-500 hover:text-red-700' />
+          <FaPinterest className='icon text-red-600 hover:text-red-800' />
+          <FaInstagram className='icon text-pink-400 hover:text-pink-600' />
+        </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
 export default Navbar;
+
+
